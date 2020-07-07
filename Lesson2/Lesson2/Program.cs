4768b99ca9428383a96db159356cc9514d18536c
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-07 18:57:22 +0300

    Add files via upload

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int images = 52;
+            int lines = images / 3;
+            int extraImages = images % 3;
+
+            Console.WriteLine("lines  = " + lines + "\nextraImages = " + extraImages);
+        }
+    }
+}
